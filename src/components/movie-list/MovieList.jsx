ca4280e1a067bd0b805b073b5b38a83125cfa978
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-09-24 18:44:44 -0300

    searchbar work, filters by year work, filters by recent work, but crash with combined filters

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -5,12 +5,18 @@ import Card from "../card/Card";
 import ReactPaginate from "react-paginate"
 
 
+
+
+
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [dataLoaded, setDataLoaded] = useState(false);
   const [page, setPage] = useState(1)
   const [keyword, setKeyword] = useState("")
+  const [year, setYear] = useState("Año")
+  const [ascdesc, setAscdesc] = useState("year.decr")
+
 
  
 
@@ -22,12 +28,34 @@ const MovieList = () => {
     setKeyword(e.target.value)
   }
 
+  const handleReset = (e) => {
+    window.location.reload()
+  }
+
+  const handleYear = (e) => {
+    setYear(e.target.value)
+  }
+
+  const handleOrder = (e) => {
+    if(e.target.value === "Reciente" ){
+      setAscdesc("year.decr")
+    }else{
+      setAscdesc("year.incr")
+    }
+  }
+
+  console.log(ascdesc);
+
+
   const fetchMoviesByName = async (keyword) => {
     try {
       const response = await axios.request({
         method: 'GET',
         url: `https://moviesdatabase.p.rapidapi.com/titles/search/title/${keyword}`,
-        params: {exact: 'false'},
+        params: {
+          exact: 'false',
+          sort: `${ascdesc}`
+        },
         headers: {
           'X-RapidAPI-Key': '011271e1efmshd2ff1ab72a2356bp1f5e61jsnf423094948a1',
           'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
@@ -51,7 +79,30 @@ const MovieList = () => {
         params: {
           startYear: '2010',
           endYear: '2023',
-          sort: 'year.decr',
+          sort: `${ascdesc}`,
+          
+        },
+        headers: {
+          'X-RapidAPI-Key': '011271e1efmshd2ff1ab72a2356bp1f5e61jsnf423094948a1',
+          'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+      });
+      setMovies(response.data)
+      setIsLoading(false);
+      setDataLoaded(true);
+    } catch (error) {
+      console.log("error al obtener películas", error);
+      setIsLoading(false);
+    }
+  }
+  
+  const fetchMoviesByYear = async (year) => {
+    try {
+      const response = await axios.request({
+        method: 'GET',
+        url: `https://moviesdatabase.p.rapidapi.com/titles`,
+        params: {
+          year: `${year}`
         },
         headers: {
           'X-RapidAPI-Key': '011271e1efmshd2ff1ab72a2356bp1f5e61jsnf423094948a1',
@@ -70,24 +121,47 @@ const MovieList = () => {
 
   //UseEffect para que cuando apenas se monte la pagina me traiga las peliculas, se actualiza cuando se modifica page.
   useEffect(() => {
-    if(!keyword){
-      fetchMovies(page)
-    }
-    fetchMoviesByName(keyword);
-  }, [page, keyword]);
+   if(keyword) {
+    fetchMoviesByName(keyword, ascdesc)
+   } else if(year!=="Año") {
+    fetchMoviesByYear(year)
+   }else{
+    fetchMovies(page)
+   }
+  }, [page, keyword, year, ascdesc]);
 
   
 
 
 
 //
-
+const years = ["Año", 2021,2022,2023]
+const ordenar = ["Ordenar", "Reciente", "Antiguo"]
  
 
   return (
     <div className="movie__list">
       <div className="searchbar-container">
       <input className="searchbar" type="text" placeholder="What do you want to watch"   onKeyDown={(e) => e.key === "Enter" && handleInput(e)}  />
+      
+       
+        <select key={year} value={year} onChange={handleYear}>
+          {
+            years.map((year)=>(
+              <option key={year} value={year}>{year}</option>
+            ))
+          }
+        </select>
+          <select key={ascdesc} value={ascdesc} onChange={handleOrder}>
+            {
+              ordenar.map((orden)=>(
+                <option key={orden} value={orden}>{orden}</option>
+              ))
+            }
+          </select>
+    
+      
+      <button onClick={handleReset}>Limpiar</button>
       </div>
       
       {isLoading ? (
